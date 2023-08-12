824e1717ffa5eca95daa6d172a30908cb1d60291
Abdul Rehman <abdulrehmannaveed@gmail.com>
2023-08-12 13:09:09 +0500

    Swagger added

--- src/auth/dtos/auth-login-dto.ts
+++ src/auth/dtos/auth-login-dto.ts
@@ -1,14 +1,17 @@
 import { IsString, MaxLength, IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class AuthLoginDto {
     
     @MaxLength(255)
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty()
     email: string;
 
    
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     password: string;
 
 }
--- src/user/controllers/user-preferences.controller.ts
+++ src/user/controllers/user-preferences.controller.ts
@@ -3,13 +3,17 @@ import {  UserPreferenceInterface } from 'src/user/interfaces';
 import { UserPreferenceService } from 'src/user/services';
 import { UserPreferenceCreateDto } from 'src/user/dtos';
 import { AuthGuard } from 'src/auth/guards';
-
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiHeader } from '@nestjs/swagger';
+@ApiTags('User')
+@ApiBearerAuth()
 @Controller()
 export class UserPreferenceController {
   constructor(
     private readonly userPrefService : UserPreferenceService
   ){}
 
+  @ApiOperation({ summary: 'Create user preference' })
+  @ApiResponse({ status: 200, description: 'Returns created user preference' })
   @UseGuards(AuthGuard)
   @Post('create-user-preference')
   async createUserPreference(@Body() preference: UserPreferenceCreateDto): Promise<UserPreferenceInterface> {
